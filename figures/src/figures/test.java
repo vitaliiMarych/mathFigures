f64ec6d7aa99aa0fd04d9cb781b3ef2b325e8827
Vitalii Marych <apiskin557@gmail.com>
2022-08-04 18:41:57 +0300

    added class triangle and class for tests

---
+++ figures/src/figures/test.java
@@ -0,0 +1,13 @@
+package figures;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		triangle tri = new triangle(5,3,6);
+		
+		tri.print();
+		
+	}
+
+}
---
+++ figures/src/figures/triangle.java
@@ -0,0 +1,31 @@
+package figures;
+
+public class triangle extends figure {
+	
+	protected int[] s;
+	
+	{
+		this.type = "triangle";
+	}
+	
+	public triangle(int a, int b, int c) {
+		s = new int[] {a,b,c};
+	}
+	
+	public void print() {
+		System.out.println("a = " + s[0] + ",b = " + s[1] + ", c = " + s[2]);
+		System.out.println("squad = " + this.calculateSquad() + ", perimeter = " + this.calculatePerimeter());
+		System.out.println("type = " + this.type);
+	}
+	
+	public double calculateSquad() {
+		double p = (s[0] + s[1] + s[2]) / 2.0;
+		
+		return Math.sqrt(p * (p - s[0]) * (p - s[1]) * (p - s[2]));
+	}
+	
+	public int calculatePerimeter() {
+		return s[0] + s[1] + s[2];
+	}
+	
+}
